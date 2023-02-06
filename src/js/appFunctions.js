62a703b903b5dd9fff716a0c7c60bdce801858e9
MisterAxolotl <55125665+Mister-Axolotl@users.noreply.github.com>
2023-02-06 11:12:08 +0100

    Update appFunctions.js

--- src/js/appFunctions.js
+++ src/js/appFunctions.js
@@ -52,10 +52,12 @@ function generatePassword(length) {
         if(checkBoxCapitalLetters.checked === true){
             characters.push('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
         }
-        for ( var i = 0; i < length; i++ ) { 
-            result.push(characters.at(Math.floor(Math.random() * characters.length))); 
+        for( var i = 0 ; i < length ; i++ ) { 
+            result.push(characters.at(Math.floor(Math.random() * characters.length)));
         }
-        return password.value = result.join('');
+        if(checkBoxNumbers.checked === false && checkBoxLowercaseLetters.checked === false && checkBoxCapitalLetters.checked === false){
+            return password.value = 'You must check something!';
+        } else return password.value = result.join('');
     } else {
         return password.value = 'Number is too big! Maximum is 1000.'
     }
